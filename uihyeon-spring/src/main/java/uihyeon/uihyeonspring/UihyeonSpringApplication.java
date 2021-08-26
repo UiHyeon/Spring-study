13ab7f6968df65345360644f661a87a4bdd34ccb
DESKTOP-SA9TD7E\admin <yh4268@gmail.com>
2021-08-26 18:40:14 +0900

    setting springboot starter

---
+++ uihyeon-spring/src/main/java/uihyeon/uihyeonspring/UihyeonSpringApplication.java
@@ -0,0 +1,13 @@
+package uihyeon.uihyeonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UihyeonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UihyeonSpringApplication.class, args);
+	}
+
+}
---
+++ uihyeon-spring/src/test/java/uihyeon/uihyeonspring/UihyeonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package uihyeon.uihyeonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UihyeonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
